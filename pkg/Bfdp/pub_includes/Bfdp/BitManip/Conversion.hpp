1774e8b374fe0fa573632ea5a24ad64e62fdd55b
Daniel Kristensen <daniel.kristensen@garmin.com>
2018-08-11 16:13:29 -0500

    Bfdp: Add bit/byte limits and count conversion
    Add to BitManip:
    * BitsPerByte
    * MaxBits
    * MaxBytes
    * BitsToBytes()
    * BytesToBits()
    
    Type: New Feature
    Signed-off-by: Daniel Kristensen <daniel.kristensen@garmin.com>

---
+++ pkg/Bfdp/pub_includes/Bfdp/BitManip/Conversion.hpp
@@ -0,0 +1,96 @@
+/**
+    BFDP BitManip Conversion Declarations
+
+    Copyright 2018, Daniel Kristensen, Garmin Ltd, or its subsidiaries.
+    All rights reserved.
+
+    Redistribution and use in source and binary forms, with or without
+    modification, are permitted provided that the following conditions are met:
+
+    * Redistributions of source code must retain the above copyright notice, this
+      list of conditions and the following disclaimer.
+
+    * Redistributions in binary form must reproduce the above copyright notice,
+      this list of conditions and the following disclaimer in the documentation
+      and/or other materials provided with the distribution.
+
+    * Neither the name of the copyright holder nor the names of its
+      contributors may be used to endorse or promote products derived from
+      this software without specific prior written permission.
+
+    THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+    AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+    IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+    DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+    FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+    DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+    SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+    CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+    OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+    OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+*/
+
+#ifndef Bfdp_BitManip_Conversion
+#define Bfdp_BitManip_Conversion
+
+// External Includes
+#include <limits>
+
+// Internal includes
+#include "Bfdp/Common.hpp"
+#include "Bfdp/ErrorReporter/Functions.hpp"
+
+namespace Bfdp
+{
+
+    namespace BitManip
+    {
+
+        //! Number of bits per byte
+        //!
+        //! @note Not using CHAR_BIT, since this is not intended to be architecture-specific
+        static SizeT const BitsPerByte = 8;
+
+        //! Max number of bits (theoretical) for bitwise operations
+        static SizeT const MaxBits = std::numeric_limits< SizeT >::max() - 7;
+
+        //! Max number of bytes (theoretical) for bitwise operations
+        static SizeT const MaxBytes = MaxBits / BitsPerByte;
+
+        //! @return The number of bytes needed to store aBits of data
+        static __inline SizeT BitsToBytes
+            (
+            SizeT const aBits
+            )
+        {
+            if( aBits > MaxBits )
+            {
+                // Even though this will technically work, the reverse conversion would fail.  So
+                // its best to avoid it in all cases.
+                BFDP_MISUSE_ERROR_M( "Bit count too large", "BitManip::BitsToBytes" );
+                return MaxBytes;
+            }
+            return ( aBits / 8 ) +
+                ( ( ( aBits & 0x7 ) != 0 ) ? 1 : 0 );
+        }
+
+        //! @return The number of bytes needed to store aBits of data
+        static __inline SizeT BytesToBits
+            (
+            SizeT const aBytes
+            )
+        {
+            if( aBytes > MaxBytes )
+            {
+                BFDP_MISUSE_ERROR_M( "Byte count too large", "BitManip::BytesToBits" );
+                return MaxBits;
+            }
+
+            return aBytes * BitsPerByte;
+        }
+
+    } // namespace BitManip
+
+} // namespace Bfdp
+
+#endif // Bfdp_BitManip_Conversion
---
+++ pkg/BfsdlTests/source/BitManipConversionTest.cpp
@@ -0,0 +1,99 @@
+/**
+    BFDP BitManip Conversion Test
+
+    Copyright 2018, Daniel Kristensen, Garmin Ltd, or its subsidiaries.
+    All rights reserved.
+
+    Redistribution and use in source and binary forms, with or without
+    modification, are permitted provided that the following conditions are met:
+
+    * Redistributions of source code must retain the above copyright notice, this
+      list of conditions and the following disclaimer.
+
+    * Redistributions in binary form must reproduce the above copyright notice,
+      this list of conditions and the following disclaimer in the documentation
+      and/or other materials provided with the distribution.
+
+    * Neither the name of the copyright holder nor the names of its
+      contributors may be used to endorse or promote products derived from
+      this software without specific prior written permission.
+
+    THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+    AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+    IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+    DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+    FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+    DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+    SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+    CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+    OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+    OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+*/
+
+// External includes
+#include "gtest/gtest.h"
+
+// Internal Includes
+#include "Bfdp/BitManip/Conversion.hpp"
+#include "BfsdlTests/MockErrorHandler.hpp"
+#include "BfsdlTests/TestUtil.hpp"
+
+namespace BfsdlTests
+{
+
+    using namespace Bfdp;
+
+    class BitManipConversionTest
+        : public ::testing::Test
+    {
+        void SetUp()
+        {
+            SetDefaultErrorHandlers();
+        }
+    };
+
+    TEST_F( BitManipConversionTest, Assumptions )
+    {
+        ASSERT_EQ( 8U, BitManip::BitsPerByte );
+        ASSERT_EQ( BitManip::MaxBits, BitManip::MaxBytes * BitManip::BitsPerByte );
+    }
+
+    TEST_F( BitManipConversionTest, BitsToBytes )
+    {
+        SetMockErrorHandlers();
+        MockErrorHandler::Workspace wksp;
+
+        ASSERT_EQ( 0U, BitManip::BitsToBytes( 0 ) );
+        ASSERT_EQ( 1U, BitManip::BitsToBytes( 1 ) );
+        ASSERT_EQ( 1U, BitManip::BitsToBytes( 7 ) );
+        ASSERT_EQ( 1U, BitManip::BitsToBytes( 8 ) );
+        ASSERT_EQ( 2U, BitManip::BitsToBytes( 9 ) );
+        ASSERT_EQ( 2U, BitManip::BitsToBytes( 16 ) );
+        ASSERT_EQ( 3U, BitManip::BitsToBytes( 17 ) );
+        ASSERT_EQ( 3U, BitManip::BitsToBytes( 24 ) );
+        ASSERT_EQ( 4U, BitManip::BitsToBytes( 25 ) );
+        ASSERT_EQ( 7U, BitManip::BitsToBytes( 56 ) );
+        ASSERT_EQ( 8U, BitManip::BitsToBytes( 57 ) );
+        ASSERT_EQ( 8U, BitManip::BitsToBytes( 64 ) );
+        ASSERT_EQ( BitManip::MaxBytes, BitManip::BitsToBytes( BitManip::MaxBits ) );
+
+        wksp.ExpectMisuseError();
+        ASSERT_EQ( BitManip::MaxBytes, BitManip::BitsToBytes( std::numeric_limits< SizeT >::max() ) );
+    }
+
+    TEST_F( BitManipConversionTest, BytesToBits )
+    {
+        SetMockErrorHandlers();
+        MockErrorHandler::Workspace wksp;
+
+        ASSERT_EQ(  0U, BitManip::BytesToBits( 0 ) );
+        ASSERT_EQ(  8U, BitManip::BytesToBits( 1 ) );
+        ASSERT_EQ( 16U, BitManip::BytesToBits( 2 ) );
+        ASSERT_EQ( 64U, BitManip::BytesToBits( 8 ) );
+        ASSERT_EQ( BitManip::MaxBits, BitManip::BytesToBits( BitManip::MaxBytes ) );
+
+        wksp.ExpectMisuseError();
+        ASSERT_EQ( BitManip::MaxBits, BitManip::BytesToBits( std::numeric_limits< SizeT >::max() ) );
+    }
+
+} // namespace BfsdlTests
